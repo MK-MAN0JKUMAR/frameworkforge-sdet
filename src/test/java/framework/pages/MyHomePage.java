b65d20c05a8ef9a4b8def46f9b0573641c63f78e
Manoj Kumar <manojkr5812@gmail.com>
2026-02-02 23:28:23 +0530

    Added WaitUtil with dynamic handle wait timing

--- src/test/java/framework/pages/MyHomePage.java
+++ src/test/java/framework/pages/MyHomePage.java
@@ -1,9 +1,20 @@
 package framework.pages;
 
+import framework.driver.DriverManager;
+import framework.utils.WaitUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class MyHomePage extends BasePageObjects {
+public class MyHomePage {
+
+	private static final Logger log = LogManager.getLogger(MyHomePage.class);
+
+	public MyHomePage() {
+		PageFactory.initElements(DriverManager.getDriver(), this);
+	}
 
 	@FindBy(xpath = "//section[@id='sidebar']//p[1]")
 	private WebElement homeText;
@@ -19,8 +30,7 @@ public class MyHomePage extends BasePageObjects {
 		}
 	}
 
-
 	public void logout() {
-		click(logoutBtn);
+		WaitUtil.click(logoutBtn, 10);
 	}
 }
